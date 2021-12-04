df63f655be16b92d99c422e319e3c80bdb594570
JanSuran03 <62071148+JanSuran03@users.noreply.github.com>
2021-12-04 21:58:19 +0100

    Full solution.

--- src/prohlidkove_okruhy/core.clj
+++ src/prohlidkove_okruhy/core.clj
@@ -4,7 +4,21 @@
             [loom.alg :as alg]))
 
 (defn read-and-process-input []
-  (->> "input.txt" slurp
-       str/split-lines))
+  (let [[[_num-crossroads num-edges _num-queries] & input] (->> "input.txt" slurp
+                                                                str/split-lines
+                                                                (map #(as-> % <>
+                                                                            (str/split <> #"\ ")
+                                                                            (map read-string <>))))]
+    (split-at num-edges input)))
 
-(defn -main [& args])
+(defn path-exists? [graph [vertex-1 vertex-2 :as _query]]
+  (if (alg/bf-path graph vertex-1 vertex-2)
+    "Cesta existuje"
+    "Cesta neexistuje"))
+
+(defn -main [& args]
+  (let [[edges queries] (read-and-process-input)
+        graph (apply digraph edges)]
+    (->> queries (map #(path-exists? graph %))
+         (str/join "\n")
+         (spit "output.txt"))))
